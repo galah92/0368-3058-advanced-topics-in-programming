c95fdc38706aa79a15b886e485f59c24291cd514
galah92 <galaharoni92@gmail.com>
2018-05-01 17:23:57 +0300

    Updated FightInfo.h to follow requirements

--- GameContainers.h
+++ GameContainers.h
@@ -43,16 +43,18 @@ private:
 
 class RPSFightInfo :  public FightInfo {
 public:
-    RPSFightInfo(Point &pos, char piece, int winner) :
+    RPSFightInfo(Point &pos, char piece1, char piece2, int winner) :
         _pos(pos),
-        _piece(piece),
+        _piece1(piece1),
+        _piece2(piece2),
         _winner(winner) { }
 	const Point& getPosition() const { return _pos; };
-	char getOpponentPiece() const { return _piece; };
+	char getPiece(int player) const { return player == 1 ? _piece1 : _piece2; };
 	int getWinner() const { return _winner; };
 private:
     Point &_pos;
-    char _piece;
+    char _piece1;
+    char _piece2;
     int _winner;
 };
 
