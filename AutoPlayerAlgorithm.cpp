112ca3899ab1ff5b41eb85f482f62f377db875ec
galah92 <galaharoni92@gmail.com>
2018-05-19 19:05:28 +0300

    fixed AutoPlayerAlgorithm method signatures

--- AutoPlayerAlgorithm.cpp
+++ AutoPlayerAlgorithm.cpp
@@ -1,51 +1,48 @@
+#include <string>
 #include "AutoPlayerAlgorithm.h"
 #include "Point.h"
-#include <string>
 
-const std::string FILES_PREFIX = "player";
-const std::string BOARD_FILE_EXT = ".rps_board";
-const std::string MOVES_FILE_EXT = ".rps_moves";
 
-void getInitialPositions(int player, std::vector<std::unique_ptr<PiecePosition>>& positions){
-    positions.clear(); // to make sure
-    // random corner?
-    // flag block
-    positions.push_back(std::make_unique<PiecePositionImpl>(0,0, 'F', ' ')); // put the flag in the corner
-    positions.push_back(std::make_unique<PiecePositionImpl>(0,1, 'B', ' ')); // cover the flag 
-    positions.push_back(std::make_unique<PiecePositionImpl>(1,0, 'B', ' ')); // cover the flag
-    positions.push_back(std::make_unique<PiecePositionImpl>(1,1, 'J', 'B')); // cover the flag
-    
-    // randomize other pieces?
-    positions.push_back(std::make_unique<PiecePositionImpl>(5,5, 'J', 'B'));
-    positions.push_back(std::make_unique<PiecePositionImpl>(2,7, 'R', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(8,2, 'R', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(2,2, 'P', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(3,7, 'P', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(4,4, 'P', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(5,8, 'P', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(7,4, 'P', ' '));
-    positions.push_back(std::make_unique<PiecePositionImpl>(6,6, 'S', ' '));
+void AutoPlayerAlgorithm::getInitialPositions(int player, std::vector<std::unique_ptr<PiecePosition>>& positions) {
+	positions.clear(); // to make sure
+					   // random corner?
+					   // flag block
+	positions.push_back(std::make_unique<PiecePositionImpl>(0, 0, 'F', ' ')); // put the flag in the corner
+	positions.push_back(std::make_unique<PiecePositionImpl>(0, 1, 'B', ' ')); // cover the flag 
+	positions.push_back(std::make_unique<PiecePositionImpl>(1, 0, 'B', ' ')); // cover the flag
+	positions.push_back(std::make_unique<PiecePositionImpl>(1, 1, 'J', 'B')); // cover the flag
+
+																			  // randomize other pieces?
+	positions.push_back(std::make_unique<PiecePositionImpl>(5, 5, 'J', 'B'));
+	positions.push_back(std::make_unique<PiecePositionImpl>(2, 7, 'R', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(8, 2, 'R', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(2, 2, 'P', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(3, 7, 'P', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(4, 4, 'P', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(5, 8, 'P', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(7, 4, 'P', ' '));
+	positions.push_back(std::make_unique<PiecePositionImpl>(6, 6, 'S', ' '));
 
 	(void)player;
 }
 
-void notifyOnInitialBoard(const Board& b, const std::vector<std::unique_ptr<FightInfo>>& fights){
-    (void) b;  
-    (void) fights;
+void AutoPlayerAlgorithm::notifyOnInitialBoard(const Board & b, const std::vector<std::unique_ptr<FightInfo>>& fights) {
+	(void)b;
+	(void)fights;
 }
 
-void notifyOnOpponentMove(const Move& move){
-    (void) move;
+void AutoPlayerAlgorithm::notifyOnOpponentMove(const Move & move) {
+	(void)move;
 }
 
-void notifyFightResult(const FightInfo& fightInfo){
-    (void) fightInfo;
+void AutoPlayerAlgorithm::notifyFightResult(const FightInfo & fightInfo) {
+	(void)fightInfo;
 }
 
-std::unique_ptr<Move> getMove(){
-    return nullptr;
+std::unique_ptr<Move> AutoPlayerAlgorithm::getMove() {
+	return nullptr;
 }
 
-std::unique_ptr<JokerChange> getJokerChange(){
-    return nullptr;
+std::unique_ptr<JokerChange> AutoPlayerAlgorithm::getJokerChange() {
+	return nullptr;
 }
--- AutoPlayerAlgorithm.h
+++ AutoPlayerAlgorithm.h
@@ -12,12 +12,12 @@
 
 class AutoPlayerAlgorithm : public PlayerAlgorithm {
 public:
-	void getInitialPositions(int player, std::vector<std::unique_ptr<PiecePosition>>& positions);
-	void notifyOnInitialBoard(const Board& b, const std::vector<std::unique_ptr<FightInfo>>& fights);
-	void notifyOnOpponentMove(const Move& move);
-	void notifyFightResult(const FightInfo& fightInfo);
-	std::unique_ptr<Move> getMove();
-	std::unique_ptr<JokerChange> getJokerChange();
+	void getInitialPositions(int player, std::vector<std::unique_ptr<PiecePosition>>& positions) override;
+	void notifyOnInitialBoard(const Board& b, const std::vector<std::unique_ptr<FightInfo>>& fights) override;
+	void notifyOnOpponentMove(const Move& move) override;
+	void notifyFightResult(const FightInfo& fightInfo) override;
+	std::unique_ptr<Move> getMove() override;
+	std::unique_ptr<JokerChange> getJokerChange() override;
 private:
-	std::array<std::shared_ptr<Piece>, N * M> _OpponentBoard;
+	std::array<std::shared_ptr<Piece>, N * M> _opponentBoard;
 };
