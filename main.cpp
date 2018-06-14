349f2518aecb4b844ee6d7ebdde44188f8ac0df9
galah92 <galaharoni92@gmail.com>
2018-06-14 23:34:18 +0300

    updated main()

--- main.cpp
+++ main.cpp
@@ -5,20 +5,16 @@
 
 
 int main(int argc, char *argv[]) {
-    unsigned int numThreads = 4;
-    std::string path = "./";
+    auto& manager = TournamentManager::getTournamentManager();
     std::vector<std::string> vec(argv + 1, argv + argc);
     vec.push_back(""); // to make is possible to itetate until vec.size() - 1
     for (unsigned int i = 0; i < vec.size() - 1; i++) {
         if (vec[i] == "-threads") {
-            numThreads = std::stoul(vec[i + 1]);
+            manager.maxThreads = std::stoul(vec[i + 1]);
         } else if (vec[i] == "-path") {
-            path = vec[i + 1];
+            manager.path = vec[i + 1];
         }
     }
-    auto& manager = TournamentManager::getTournamentManager();
-    manager.maxThreads = numThreads;
-    manager.path = path;
     manager.run();
     return 0;
 }
