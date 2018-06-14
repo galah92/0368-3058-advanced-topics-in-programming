cd9ff5db326de0ac1814eb36fb91dce4db977796
galah92 <galaharoni92@gmail.com>
2018-06-14 15:47:12 +0300

    fixed to GameBoard<T>

--- GameBoard.h
+++ GameBoard.h
@@ -16,15 +16,15 @@ public:
         T piece;
         int player;
     };
-    void clear();
-    int getPlayer(const Point& pos) const override;
-    bool isValid(const Point& pos) const;
-    Entry& operator[](const Point& pos);
-    const Entry& operator[](const Point& pos) const;
-    Entry& operator[](const std::pair<int, int> pos);
-    const Entry& operator[](const std::pair<int, int> pos) const;
+    void clear() { _arr.fill({ T(), 0 }); }
+    int getPlayer(const Point& pos) const override { return _arr[getIndex(pos)].player; }
+    bool isValid(const Point& pos) const { return getIndex(pos) >= 0 && getIndex(pos) < SIZE; }
+    Entry& operator[](const Point& pos) { return _arr[getIndex(pos)]; }
+    const Entry& operator[](const Point& pos) const { return _arr[getIndex(pos)]; }
+    Entry& operator[](const std::pair<int, int> pos) { return _arr[getIndex(pos)]; }
+    const Entry& operator[](const std::pair<int, int> pos) const { return _arr[getIndex(pos)]; }
 private:
-    std::size_t getIndex(const Point& pos);
+    std::size_t getIndex(const Point& pos) const { return pos.getX() * M + pos.getY(); }
     const static std::size_t SIZE = N * M;
     std::array<Entry, SIZE> _arr;
 };
